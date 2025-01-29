aa35777f8e623bf6fee0429a70bfa7c057682e37
Kaitlyn <ksmetler@gmail.com>
2025-01-29 08:19:00 -0700

    Start Game

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿//Make a Tic Tac Toe Game
+//Welcome user to the game
+Console.WriteLine("Welcome to Tic Tac Toe!")
+
+//Create game board array to store players' choices
+
+//Ask each player in turn for their choice and update the game board array
+
+//Print the board by calling method in supporting class
+
+//Check for winner by calling method, notify palyers when a win has occurred
